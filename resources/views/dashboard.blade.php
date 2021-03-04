2e160abdc4a4c2c1717f95ae5293b256e5bb2e35
fpurnahar <fadhil.purnahar@gmail.com>
2021-03-05 00:02:43 +0700

    update template

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,64 @@
 
 @section('content')
     
-    <h1>hellow</h1>
+    <div class="col">
+        <!-- Profile Image -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">About Me</h3>
+              </div>
+              <div class="card-body box-profile">
+              <div class="text-center">
+                  <img class="profile-user-img img-fluid img-circle" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}">
+                </div>
+                <h3 class="profile-username text-center">{{ Auth::user()->name }}</h3>
+                    <p class="text-muted text-center">Backend Developer</p>
+                <a href="https://www.instagram.com/fpurnahar" target="_blank" class="btn btn-primary btn-block"><b>Follow Instagram</b></a>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+    </div> 
+
+    <div class="col">
+            <!-- About Me Box -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">About Me</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <strong><i class="fas fa-book mr-1"></i> Education</strong>
+
+                <p class="text-muted">
+                 Polytechnic State Lhokseumawe
+                </p>
+
+                <hr>
+
+                <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
+
+                <p class="text-muted">Lhokseumawe, Aceh</p>
+
+                <hr>
+
+                <strong><i class="fas fa-pencil-alt mr-1"></i> Skills</strong>
+
+                <p class="text-muted">
+                  <span class="tag tag-danger">UI Design,</span>
+                  <span class="tag tag-success">Html, Css , Sass</span>
+                  <span class="tag tag-info">Javascript</span>
+                  <span class="tag tag-warning">,PHP</span>
+                  <span class="tag tag-primary">,CodeIgniter</span>
+                  <span class="tag tag-primary">,Laravek</span>
+                  <span class="tag tag-primary">,Node.js</span>
+                  <span class="tag tag-primary">,React.js</span>
+                </p>
+                </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+    </div>
 
 @endsection
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,24 +11,15 @@
     href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{url ('assets')}}/plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="{{url ('assets')}}/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="{{url ('assets')}}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="{{url ('assets')}}/plugins/jqvmap/jqvmap.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{{url ('assets')}}/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="{{url ('assets')}}/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="{{url ('assets')}}/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{url ('assets')}}/dist/css/adminlte.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{url ('assets')}}/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{url ('assets')}}/plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="{{url ('assets')}}/plugins/summernote/summernote-bs4.min.css">
 
   @yield('link')
+  
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -65,9 +56,7 @@
         <div class="container-fluid">
           <!-- Small boxes (Stat box) -->
           <div class="row">
-            <div class="col">
-                @yield('content')
-            </div>
+            @yield('content')
           </div>
           <!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -95,20 +84,43 @@
   
   <!-- jQuery -->
   <script src="{{url ('assets')}}/plugins/jquery/jquery.min.js"></script>
-  <!-- jQuery UI 1.11.4 -->
-  <script src="{{url ('assets')}}/plugins/jquery-ui/jquery-ui.min.js"></script>
-  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-  <script>
-    $.widget.bridge('uibutton', $.ui.button)
-  </script>
   <!-- Bootstrap 4 -->
   <script src="{{url ('assets')}}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- Tempusdominus Bootstrap 4 -->
-  <script src="{{url ('assets')}}/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-  <!-- overlayScrollbars -->
-  <script src="{{url ('assets')}}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <!-- DataTables  & Plugins -->
+  <script src="{{url ('assets')}}/plugins/datatables/jquery.dataTables.min.js"></script>
+  <script src="{{url ('assets')}}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <script src="{{url ('assets')}}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+  <script src="{{url ('assets')}}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+  <script src="{{url ('assets')}}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+  <script src="{{url ('assets')}}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+  <script src="{{url ('assets')}}/plugins/jszip/jszip.min.js"></script>
+  <script src="{{url ('assets')}}/plugins/pdfmake/pdfmake.min.js"></script>
+  <script src="{{url ('assets')}}/plugins/pdfmake/vfs_fonts.js"></script>
+  <script src="{{url ('assets')}}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+  <script src="{{url ('assets')}}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+  <script src="{{url ('assets')}}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
   <!-- AdminLTE App -->
-  <script src="{{url ('assets')}}/dist/js/adminlte.js"></script>
+  <script src="{{url ('assets')}}/dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="{{url ('assets')}}/dist/js/demo.js"></script>
+  <!-- Page specific script -->
+  <script>
+    $(function () {
+      $("#example1").DataTable({
+        "responsive": true, "lengthChange": false, "autoWidth": false,
+        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+      $('#example2').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+      });
+    });
+  </script>
 
   @yield('script')
 </body>
